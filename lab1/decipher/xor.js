3f3839736abe98a2df08432d678527f4896f7c88
Nikita Kemarskiy <nikitakemarskiy@gmail.com>
2021-10-20 13:34:16 +0300

    LAB-1: Update 2-nd task implementation

--- lab1/decipher/xor.js
+++ lab1/decipher/xor.js
@@ -1,6 +1,15 @@
 const KEYS = [...new Array(256).keys()];
 
-const getAllPossibleKeysWithDecipheredTexts = (text) => KEYS.map((key) => [...text].reduce(
-  (accum, [char]) => accum + String.fromCharCode(char.charCodeAt() ^ key),
-  ''
-));
+const getAllPossibleKeysWithDecipheredTexts = (text) => KEYS.map(
+  (key) => [
+    key.toString(2),
+    [...text].reduce(
+      (accum, [char]) => accum + String.fromCharCode(char.charCodeAt() ^ key),
+      ''
+    )
+  ]
+);
+
+module.exports = {
+  getAllPossibleKeysWithDecipheredTexts,
+};
