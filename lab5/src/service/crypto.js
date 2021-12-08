fc5d5e47e738092c7e9c7bbf7cf227f8525b2122
Nikita Kemarskiy <nikitakemarskiy@gmail.com>
2021-12-08 11:42:23 +0200

    LAB-5: Fix user, crypto services (encryption / decryption of user data is working now)

--- lab5/src/service/crypto.js
+++ lab5/src/service/crypto.js
@@ -7,23 +7,27 @@ function encryptObjectFieldsWithAead(obj) {
   return _.mapValues(obj, encryptWithAead);
 }
 
+function decryptObjectFieldsWithAead(obj) {
+  return _.mapValues(obj, decryptWithAead);
+}
+
 function encryptWithAead(value) {
   const nonceAsUint8Array = tweetnacl.randomBytes(tweetnacl.secretbox.nonceLength);
   const xsalsa20Poly1305Box = tweetnacl.secretbox(
     tweetnaclUtil.decodeUTF8(value),
-    nonce,
+    nonceAsUint8Array,
     tweetnaclUtil.decodeUTF8(config.db.key),
   );
 
-  const valueWithNonce = new Uint8Array(nonceAsUint8Array.length + xsalsa20Poly1305Box.length);
-  valueWithNonce.set(nonceAsUint8Array);
-  valueWithNonce.set(xsalsa20Poly1305Box, nonceAsUint8Array.length);
+  const valueWithNonceAsUint8Array = new Uint8Array(nonceAsUint8Array.length + xsalsa20Poly1305Box.length);
+  valueWithNonceAsUint8Array.set(nonceAsUint8Array);
+  valueWithNonceAsUint8Array.set(xsalsa20Poly1305Box, nonceAsUint8Array.length);
 
-  return tweetnaclUtil.encodeBase64(valueWithNonce);
+  return tweetnaclUtil.encodeBase64(valueWithNonceAsUint8Array);
 }
 
 function decryptWithAead(valueWithNonce) {
-  const valueWithNonceAsUint8Array = decodeBase64(valueWithNonce);
+  const valueWithNonceAsUint8Array = tweetnaclUtil.decodeBase64(valueWithNonce);
   const nonceAsUint8Array = valueWithNonceAsUint8Array.slice(0, tweetnacl.secretbox.nonceLength);
   const valueAsUint8Array = valueWithNonceAsUint8Array.slice(
     tweetnacl.secretbox.nonceLength,
@@ -41,6 +45,7 @@ function decryptWithAead(valueWithNonce) {
 
 module.exports = {
   encryptObjectFieldsWithAead,
+  decryptObjectFieldsWithAead,
   encryptWithAead,
   decryptWithAead,
 };
--- lab5/src/service/user.js
+++ lab5/src/service/user.js
@@ -1,5 +1,6 @@
-const { SHA3 } = require('sha3');
+const _ = require('lodash');
 const argon2 = require('argon2');
+const { SHA3 } = require('sha3');
 const cryptoService = require('./crypto');
 const model = require('../model');
 
@@ -7,6 +8,26 @@ const TIME_COST = 2;
 const MEMORY_COST = 15360;
 const PARALELLISM = 1;
 const HASH_SIZE = 512;
+const USER_FIELDS_TO_ENCRYPT = [
+  'phoneNumber',
+  'address',
+  'passwordHash',
+];
+
+function encryptUserData(userData) {
+  return {
+    ...userData,
+    ...cryptoService.encryptObjectFieldsWithAead(_.pick(userData, USER_FIELDS_TO_ENCRYPT)),
+  };
+}
+
+function decryptUser(user) {
+  const decryptedUserFields = cryptoService.decryptObjectFieldsWithAead(_.pick(user, USER_FIELDS_TO_ENCRYPT));
+
+  Object.entries(decryptedUserFields).map(([key, value]) => user[key] = value);
+
+  return user;
+}
 
 async function createUser({
   login,
@@ -14,43 +35,47 @@ async function createUser({
   phoneNumber,
   address
 }) {
-  const encryptedUserFields = encryptObjectFieldsWithAead({
-    login,
-    phoneNumber,
-    address
-  });
   const passwordHash = await getPasswordHash(password);
 
-  return model.user.create({
+  return model.user.create(encryptUserData({
+    login,
     passwordHash,
-    ...encryptedUserFields,
+    phoneNumber,
+    address,
+  }));
+}
+
+async function getUserByLogin(login) {
+  const user = await model.user.findOne({
+    where: { login },
   });
+
+  return decryptUser(user);
 }
 
 async function getPasswordHash(password) {
   const sha3Hash = new SHA3(HASH_SIZE).update(password).digest('utf-8');
-  const argon2Hash = await argon2.hash(sha3Hash, {
+
+  return argon2.hash(sha3Hash, {
     type: argon2.argon2id,
     timeCost: TIME_COST,
     memoryCost: MEMORY_COST,
     parallelism: PARALELLISM
   });
-  return cryptoService.encryptWithAead(argon2Hash);
 }
 
 async function authenticate(login, password) {
-  const user = await model.user.findOne({
-    where: { login },
-  });
+  const user = await getUserByLogin(login);
 
   if (!user) {
     return false;
   }
 
   const sha3Hash = new SHA3(HASH_SIZE).update(password).digest('utf-8');
-  const argon2Hash = cryptoService.decryptWithAead(user.passwordHash);
 
-  return argon2.verify(argon2Hash, sha3Hash);
+  console.dir(user);
+
+  return argon2.verify(user.passwordHash, sha3Hash);
 }
 
 module.exports = {
